5b1ac167f03266b1a5bee72251c66cb19e3df703
David Almeida <davidalmeidadrive@gmail.com>
2020-02-08 13:32:33 -0300

    entities

---
+++ src/enitities/Department.java
@@ -0,0 +1,18 @@
+package enitities;
+
+public class Department {
+	
+	private String name;
+
+	public Department(String name) {
+		this.name = name;
+	}
+	
+	
+	public String getName() {
+		return name;
+	}
+	
+	
+
+}
